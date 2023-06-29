12f009cec27a4f7d9dc9925271d3f2bafd7e8b12
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-06-29 14:10:39 -0300

    core: clean old code

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,53 +1,9 @@
 import { useSelector, TypedUseSelectorHook } from 'react-redux'
-
-//  ------------- New --------------
-
-import { configureStore, createSlice } from '@reduxjs/toolkit'
-
-const todoSlice = createSlice({
-  name: 'todo',
-  initialState: ['Fazer café', 'Estudar Redux', 'Estudar Zustand'],
-
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo)
-    },
-  },
-})
+import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice.reducer,
-  },
+  reducer: {},
 })
-
-export const { add } = todoSlice.actions
-
-//  ----------- Legacy ----------------
-
-// import { legacy_createStore as createStore } from 'redux'
-
-// function counterReducer(
-//   state = { todo: ['Fazer café', 'Estudar Redux', 'Estudar Zustand'] },
-//   action: any,
-// ) {
-//   switch (action.type) {
-//     case 'todo/add':
-//       return {
-//         todo: [...state.todo, action.payload.newTodo],
-//       }
-//     default:
-//       return state
-//   }
-// }
-
-// export const store = createStore(counterReducer)
-
-// export function add(payload: any) {
-//   return { type: 'todo/add', payload }
-// }
-
-//  ---------------------------
 
 export type RootState = ReturnType<typeof store.getState>
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
