b0dd7a3daa371347565a886a42af9afb4aa26315
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-06-30 15:07:39 -0300

    feat: loader indicator

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,11 @@ import { useCurrentLesson } from '../store/slices/player'
 export function Header() {
   const { currentLesson, currentModule } = useCurrentLesson()
 
-  if (!currentLesson || !currentModule) {
-    return null
-  }
-
   return (
     <header className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
+      <h1 className="text-2xl font-bold">{currentLesson?.title}</h1>
       <span className="text-sm text-zinc-400">
-        Módulo &quot;{currentModule.title}&quot;
+        {currentModule?.title ? `Módulo "${currentModule?.title}"` : ''}
       </span>
     </header>
   )
